7a9095bf1a1650f826f79c80217a15e6e709b265
ho1ger <drscheme2017@gmail.com>
2017-08-04 20:52:07 +0200

    better unittests

--- src/test/java/Hello/HelloTest.java
+++ src/test/java/Hello/HelloTest.java
@@ -3,6 +3,8 @@ package Hello;
 import org.junit.Test;
 
 import java.lang.reflect.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static junit.framework.TestCase.assertTrue;
 
@@ -10,12 +12,15 @@ public class HelloTest{
 
     public HelloTest() {
     }
-
+    
     @Test
     public void testTime() throws Exception {
         Hello hello = new Hello();
         String time = hello.tellTime();
+        Pattern p = Pattern.compile("[A-Za-z: ]+[\\d]{2}:[\\d]{2}:[\\d]{2}.[\\d]{1,3}");
+        Matcher m = p.matcher(time);
         assertTrue(time.length() > 0);
+        assertTrue(m.matches() == true);
     }
 
     @Test
@@ -33,5 +38,7 @@ public class HelloTest{
         String output = hello.doGreet();
         assertTrue(input == output);
     }
+
+
 }
 
